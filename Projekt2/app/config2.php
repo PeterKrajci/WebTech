94a4053a9f949df8b6d2dfc50b192cd8a7503a99
PeterKrajci <73019481+PeterKrajci@users.noreply.github.com>
2021-11-11 21:20:05 +0100

    Delete Projekt2 directory

--- Projekt2/app/config2.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('SERVERNAME', 'localhost');
-define('USERNAME', '*****');
-define('PASSWORD','*****');
-define('DBNAME','rest');
--- Projekt2/js/jQuery.serializeObject-master/jquery.serializeObject.d.ts
+++
@@ -1,4 +0,0 @@
-
-interface JQuery {
-    serializeObject(): { [index: string]: string|string[] }
-}
